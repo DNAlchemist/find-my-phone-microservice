ad5d487b8fcb22cd41fc284cff5ccbeed682fff4
Ruslan Mikhalev <RMikhalev@alfabank.ru>
2018-01-22 16:41:20 +0300

    Add docker support

--- src/main/resources/GebConfig.groovy
+++ src/main/resources/GebConfig.groovy
@@ -1,13 +1,42 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Mikhalev Ruslan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.remote.RemoteWebDriver
 
-driver = {
-    DesiredCapabilities capabilities = DesiredCapabilities.chrome()
+DesiredCapabilities capabilities = DesiredCapabilities.chrome()
+capabilities.setCapability(ChromeOptions.CAPABILITY, new ChromeOptions()
+        .addArguments("headless")
+)
 
-    capabilities.setCapability(ChromeOptions.CAPABILITY, new ChromeOptions()
-            .addArguments("headless")
-    )
-
-    return new ChromeDriver(capabilities)
+if (System.getProperty("webdriver.chrome.driver")) {
+    driver = {
+        new ChromeDriver(capabilities)
+    }
+} else {
+    driver = {
+        new RemoteWebDriver(new URL("http://chrome:4444/wd/hub"), capabilities)
+    }
 }
